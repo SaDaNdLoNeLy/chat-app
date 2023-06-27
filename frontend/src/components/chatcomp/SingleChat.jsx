a0a675c36800b0589cf0e8c93e8e20f81ee29cb8
Lehung59 <nguyenlehungsc@gmail.com>
2023-06-27 11:06:53 +0700

    fixx

--- frontend/src/components/chatcomp/SingleChat.jsx
+++ frontend/src/components/chatcomp/SingleChat.jsx
@@ -8,6 +8,7 @@ import io from "socket.io-client";
 import { selectClasses } from "@mui/material";
 import Home from "../callcomp/home";
 
+
 const getSender = (loggedUser, users) => {
   return users[0]._id === loggedUser._id
     ? users[0].username
